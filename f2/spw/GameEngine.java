08981ee9628aeb1a03115e371cbcccee420a5cfe
baiteuyy <antika_y@hotmail.com>
2015-04-16 12:24:44 +0700

    *03* เพิ่ม enemy2

--- f2/spw/GameEngine.java
+++ f2/spw/GameEngine.java
@@ -14,7 +14,11 @@ import javax.swing.Timer;
 public class GameEngine implements KeyListener, GameReporter{
 	GamePanel gp;
 		
-	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
+	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+
+	//add Enemy2	
+	private ArrayList<Enemy2> enemies2 = new ArrayList<Enemy2>();
+
 	private SpaceShip v;	
 	
 	private Timer timer;
@@ -49,6 +53,13 @@ public class GameEngine implements KeyListener, GameReporter{
 		enemies.add(e);
 	}
 	
+	 // add generateEnemy2
+	private void generateEnemy2(){
+		Enemy2 e = new Enemy2((int)(Math.random()*390), 30);
+		gp.sprites.add(e);
+		enemies2.add(e);
+	}
+	
 	private void process(){
 		if(Math.random() < difficulty){
 			generateEnemy();
@@ -79,6 +90,37 @@ public class GameEngine implements KeyListener, GameReporter{
 		}
 	}
 	
+	// add process2
+	private void process2(){
+		if(Math.random() < difficulty){
+			generateEnemy2();
+		}
+		
+		Iterator<Enemy2> e_iter = enemies2.iterator();
+		while(e_iter.hasNext()){
+			Enemy2 e = e_iter.next();
+			e.proceed();
+			
+			if(!e.isAlive()){
+				e_iter.remove();
+				gp.sprites.remove(e);
+				score += 100;
+			}
+		}
+		
+		gp.updateGameUI(this);
+		
+		Rectangle2D.Double vr = v.getRectangle();
+		Rectangle2D.Double er;
+		for(Enemy2 e : enemies2){
+			er = e.getRectangle();
+			if(er.intersects(vr)){
+				die();
+				return;
+			}
+		}
+	}
+
 	public void die(){
 		timer.stop();
 	}
